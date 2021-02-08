badfa2e2a064df5a9d24184257b65e546f61c062
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-08 14:10:44 +0100

    ajoute component/index.js

---
+++ src/front/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+import Layout from "./components/Layout"
+// import './assets/fonts/linearicons/style.css'
+// import './assets/admin.css'
+ReactDOM.render(
+    <Layout/>, container
+)

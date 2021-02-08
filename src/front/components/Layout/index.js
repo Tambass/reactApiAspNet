939c1b20b1d605ff4c5cab6c33fcdca4be437966
FabienFaguet <58740890+Tambass@users.noreply.github.com>
2021-02-08 15:02:55 +0100

    configure antDesign

--- src/front/components/Layout/index.js
+++ src/front/components/Layout/index.js
@@ -1,14 +1,50 @@
 import React from "react";
-export default class Layout extends React.Component {
+import { Form, Input, Button, Layout } from 'antd';
+import './accueil.css';
+
+const { Header, Footer, Content } = Layout;
+export default class Accueil extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
       }
     }
     render() {
+        const submitForm = (values)=> {
+            // console.log(values);
+        }
         //
       return (
-        <>Ici le layout</>
+          <>
+            <Layout>
+                <Header>Header</Header>
+                <Content>
+                    <Form onFinish={submitForm}>
+                    {/* produit */}
+                    <Form.Item
+                    label="Produit"
+                    name="name"
+                    >
+                    <Input />
+                    </Form.Item>
+                    {/* prix */}
+                    <Form.Item
+                    label="Prix"
+                    name="price"
+                    >
+                    <Input />
+                    </Form.Item>
+
+                    <Button type="primary" htmlType="submit">
+                    Enregistrer
+                    </Button>
+
+                </Form>
+                </Content>
+                <Footer>Footer</Footer>
+            </Layout>
+            
+          </>
       ) // Code HTML
     }
 }
